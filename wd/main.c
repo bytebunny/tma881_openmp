52054ea0f7ecd6b3848ce65a9fc70c98d6e852bb
Da Wu <dawu@chalmers.se>
2019-10-12 20:17:55 +0200

    fix a bug on critical

--- wd/main.c
+++ wd/main.c
@@ -10,14 +10,15 @@ main(int argc, char* argv[])
     n_threads = strtol(++ptr, NULL, 10);
   }
   omp_set_num_threads(n_threads);
-  char filnam[]="cell_50";
+  char filnam[] = "cell_50";
   FILE* fp;
   fp = fopen(filnam, "r");
   fseek(fp, 0L, SEEK_END);
   long res = ftell(fp);
-  fclose(fp);
+  fseek(fp, 0L, SEEK_SET);
+  // fclose(fp);
   size_t total_lines = res / 24;
-  size_t max_load_lines = 10;
+  size_t max_load_lines = 1000;
   // size_t slice_num = total_lines / max_load_lines; // now i have 5 slices
   // printf("lines %ld\n",total_lines);
 
@@ -47,12 +48,13 @@ main(int argc, char* argv[])
       // done_end_blk = 0;
     }
 
-    printf("\033[0;35m");
-    printf("start line: %ld\n", ix);
-    printf("\033[0m");
-    printf("\033[0;36m");
-    printf("------------\n");
-    printf("\033[0m");
+    /*     printf("\033[0;35m");
+        printf("start line: %ld\n", ix);
+        printf("\033[0m");
+        printf("\033[0;36m");
+        printf("------------\n");
+        printf("\033[0m") */
+    ;
     // printf("<<task load starter end, thds: %d\n\n", omp_get_thread_num());
     // compute how many blocks need
     size_t now_read = ix + 1;
@@ -84,59 +86,64 @@ main(int argc, char* argv[])
           // printf("task load ender start, thds: %d\n", omp_get_thread_num());
           char par_line[24];
           size_t read_line_num = line_offset + ixc;
-          FILE* fl = fopen(filnam, "r");
-          fseek(fl, (read_line_num)*24 * sizeof(char), SEEK_SET);
-          if (fread(par_line, 1, 24, fl) == 24) {
-            printf("\033[0;32m");
-            printf("reading line: %ld, thd: %d\n",
-                   read_line_num,
-                   omp_get_thread_num());
-            printf("\033[0m");
-            double end_pnt[3];
-            for (size_t jx = 0; jx < 3; jx++) {
-              char nums[7];
-              nums[0] = par_line[8 * jx + 0];
-              nums[1] = par_line[8 * jx + 1];
-              nums[2] = par_line[8 * jx + 2];
-              nums[3] = par_line[8 * jx + 4];
-              nums[4] = par_line[8 * jx + 5];
-              nums[5] = par_line[8 * jx + 6];
-              nums[6] = '\0';
-              int number = naive_str2l(nums);
-              end_pnt[jx] = (double)number / 1000.0;
-              //#pragma omp criticle
-              //            printf("%s\n", nums);
-              // printf("%4.4f\n", end_pnt[jx]);
-            }
-
-            double total_len_2 =
-              (header_pnt[0] - end_pnt[0]) * (header_pnt[0] - end_pnt[0]) +
-              (header_pnt[1] - end_pnt[1]) * (header_pnt[1] - end_pnt[1]) +
-              (header_pnt[2] - end_pnt[2]) * (header_pnt[2] - end_pnt[2]);
-
-            size_t total_len_rnd = (size_t)(sqrt(total_len_2) * 100.0 + 0.5);
-            counting[total_len_rnd] += 1;
-            //   printf("%4.2f %ld\n",
-            //          (double)total_len_rnd / 100.0,
-            //          counting[total_len_rnd]);
+// FILE* fl = fopen(filnam, "r");
+#pragma omp critical
+          {
+            fseek(fp, read_line_num * 24 * sizeof(char), SEEK_SET);
+            fread(par_line, 1, 24, fp);
           }
-          fclose(fl);
-          //#pragma omp criticle
+          // if (fread(par_line, 1, 24, fp) == 24) {
+          /*             printf("\033[0;32m");
+                      printf("reading line: %ld, thd: %d\n",
+                             read_line_num,
+                             omp_get_thread_num());
+                      printf("\033[0m"); */
+          double end_pnt[3];
+          for (size_t jx = 0; jx < 3; jx++) {
+            char nums[7];
+            nums[0] = par_line[8 * jx + 0];
+            nums[1] = par_line[8 * jx + 1];
+            nums[2] = par_line[8 * jx + 2];
+            nums[3] = par_line[8 * jx + 4];
+            nums[4] = par_line[8 * jx + 5];
+            nums[5] = par_line[8 * jx + 6];
+            nums[6] = '\0';
+            int number = naive_str2l(nums);
+            end_pnt[jx] = (double)number / 1000.0;
+            //#pragma omp critical
+            //            printf("%s\n", nums);
+            // printf("%4.4f\n", end_pnt[jx]);
+          }
+
+          double total_len_2 =
+            (header_pnt[0] - end_pnt[0]) * (header_pnt[0] - end_pnt[0]) +
+            (header_pnt[1] - end_pnt[1]) * (header_pnt[1] - end_pnt[1]) +
+            (header_pnt[2] - end_pnt[2]) * (header_pnt[2] - end_pnt[2]);
+
+          size_t total_len_rnd = (size_t)(sqrt(total_len_2) * 100.0 + 0.5);
+          counting[total_len_rnd] += 1;
+          //   printf("%4.2f %ld\n",
+          //          (double)total_len_rnd / 100.0,
+          //          counting[total_len_rnd]);
+          //}
+          // fclose(fl);
+          //#pragma omp critical
           //        printf("done_end_blk: %ld\n", done_end_blk);
-          //#pragma omp criticle
+          //#pragma omp critical
           //        printf("<<task load ender end, thds: %d\n",
           //        omp_get_thread_num());
         }
       } // parallel end
-      printf("\033[0;34m");
-      printf("------------\n");
-      printf("\033[0m");
+      /*       printf("\033[0;34m");
+            printf("------------\n");
+            printf("\033[0m") */
+      ;
       // done_end_blk++;
     } // block loop
-    printf("\n");
-  } // end of ix loop
+      /*     printf("\n"); */
+  }   // end of ix loop
   // load target
-  //fclose(fp);
+  fclose(fp);
 
   for (size_t ixb = 0; ixb < 3465; ixb++) {
     if (counting[ixb] > 0) {
